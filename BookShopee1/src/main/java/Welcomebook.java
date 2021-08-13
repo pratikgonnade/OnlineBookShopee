79a20803a6a1dc7c4e7dd966de2fc9199c837a34
Pratik Gonnade <pratikgonnade@gmail.com>
2021-08-13 12:46:39 +0530

    Database Connected

--- BookShopee1/src/main/java/Welcomebook.java
+++ BookShopee1/src/main/java/Welcomebook.java
@@ -1,13 +1,19 @@
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Welcomebook")
+@WebServlet("/WelcomeServlet")
 public class Welcomebook extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -22,9 +28,32 @@ public class Welcomebook extends HttpServlet {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("Driver Loaded Welcome Pages");
 			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookshopee","root","admin");
+			System.out.println("Connection Created ");
+			
+			Statement stm = con.createStatement();
+			
+			ResultSet rs = 	stm.executeQuery("Select * from books");
+			
+			while(rs.next()) {
+				
+				
+					System.out.print(rs.getString("Books_ID")+" | ");
+					System.out.print(rs.getString("Books_name")+" | ");
+					System.out.print(rs.getString("Author")+" | ");
+					System.out.print(rs.getInt("Price")+" | ");
+					System.out.println();
+					
+			}
+			
+			
+			con.close();
 			
 			
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
